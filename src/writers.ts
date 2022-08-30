6f30d63f8a64bbb2301bd80b76e698df55261104
Drew S <segdrew328@gmail.com>
2022-08-30 17:45:17 -0400

    bug fixes. added aother test script.

--- src/writers.ts
+++ src/writers.ts
@@ -2,7 +2,7 @@ import { writeFile } from "fs";
 import { stringify } from "csv-stringify";
 import { Course } from "./Course";
 
-const genFileName = "catalog.json";
+const genFileName = "catalog";
 
-export const writeJSON = (courses: Course[]) => writeFile(genFileName, JSON.stringify(courses, undefined, 1), () => {});
-export const writeCSV = (data: Course[]) => stringify(data, { header: true }, (undefined, output) => writeFile(__dirname + genFileName, output, () => {}));
+export const writeJSON = (courses: Course[]) => writeFile(genFileName + ".json", JSON.stringify(courses, undefined, 1), () => {});
+export const writeCSV = (data: Course[]) => stringify(data, { header: true }, (undefined, output) => writeFile(genFileName + ".csv", output, () => {}));
